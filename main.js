43c9e17d1893fee23de65c6c3c85496ce972ad45
Elyes Hachem <elyeshm@gmail.com>
2015-05-05 23:33:21 +0200

    port 3000 test

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.render('index.ejs');
 });
 
-var server = app.listen(8080, function(){
+var server = app.listen(3000, function(){
 	console.log("The server has been launched");
 });
