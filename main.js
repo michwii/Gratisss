54dfb8aa5f856dd3cfca265da6320a907b37a9ef
Elyes Hachem <elyeshm@gmail.com>
2015-05-10 14:31:14 +0200

    Ajout de la page d'inscription

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.render(__dirname + '/view/index.ejs');
 });
 
+app.get('/inscription', function (req, res) {
+  res.render(__dirname + '/view/inscription.ejs');
+});
+
 var server = app.listen(80, function(){
 	console.log("The server has been launched");
 });
