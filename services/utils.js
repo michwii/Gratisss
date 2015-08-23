c6e03ca6e96c1e05f77d39d2eebc01bb08736402
Elyes Hachem <elyeshm@gmail.com>
2015-08-24 00:48:28 +0100

    Site map auto generation added

--- services/utils.js
+++ services/utils.js
@@ -1,3 +1,5 @@
+var request = require("request");
+
 exports.validateEmail = function validateEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
@@ -24,3 +26,4 @@ exports.getCleanUrl = function(url){
             .replace(/[\/_| -]+/g, '-');
 	return clean;
 };
+
